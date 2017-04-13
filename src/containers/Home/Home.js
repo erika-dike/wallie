3ad1365488e8283def955ab3c15ebf06b6727213
andela-cdike <chukwuerika.dike@andela.com>
2017-04-13 14:54:54 +0100

    Write html and css for the profile card pane

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Col,
+  Grid,
+  ListGroup,
+  ListGroupItem,
+  Row,
+  Well,
+} from 'react-bootstrap';
+
+import {
+  ProfileCard,
+} from './components';
+
+// css
+import './Home.css';
+
+
+const Home = () =>
+  <Grid>
+    <Row className="show-grid">
+      <Col xs={8} md={6} mdPush={3}>
+        <section className="message-section">
+          <ListGroup>
+            <ListGroupItem>
+              <Well>I am in a Well man</Well>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Well>I am in a Well man</Well>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Well>I am in a Well man</Well>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Well>I am in a Well man</Well>
+            </ListGroupItem>
+          </ListGroup>
+        </section>
+      </Col>
+      <Col xs={4} md={3} mdPull={6}>
+        <section className="module profile-section">
+          <ProfileCard />
+        </section>
+      </Col>
+      <Col xs={4} md={3}>
+        <section className="module top-post-section" />
+      </Col>
+      <Col xs={4} md={3} mdPush={3}>
+        <section className="module top-authors-section" />
+      </Col>
+    </Row>
+  </Grid>;
+
+export default Home;
